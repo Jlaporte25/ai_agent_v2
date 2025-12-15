811be00c51de57f3dfd6a26f4b87aa7307c64f71
Jlaporte25 <jacoblaporte25@gmail.com>
2025-12-15 16:14:52 -0500

    now let's you add user input

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from google import genai
 
+
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-        
-    response = client.models.generate_content(
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.user_prompt,
     )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
